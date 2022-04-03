4a7a1dd320d6a0c3e653758ec770ff7e718cbf2d
xihangyu <xihangyu@umich.edu>
2022-04-02 21:12:48 -0400

    msg

--- inekf_imu_camera.py
+++ inekf_imu_camera.py
@@ -18,8 +18,6 @@ class Right_IEKF:
         self.R_c = system.R_c  # state vector
         self.p_c = system.p_c  # state covariance
 
-        self.error_estimated = system.error_estimated
-
         self.P = system.P
 
     def Ad(self):
@@ -55,14 +53,18 @@ class Right_IEKF:
         b_w = self.b_w
         b_a = self.b_a
 
-
+        print("husky.R: ", self.R)
+        print("system.w: ", system.w)
+        print("system.a: ", system.a)
+        print("system.g: ", system.g)
+        print("husky.p: ", self.p)
+        print("R @ (system.a - b_a) + system.g: ", R @ (system.a - b_a) + system.g)
         self.R = R @ expm(skew(system.w - b_w)*system.dt)
         self.v = v + R @ (system.a - b_a) * system.dt + system.g * system.dt
-        self.p = p + v * system.dt + 0.5 * R @ (system.a - b_a) * system.dt * system.dt + 0.5 * system.g * system.dt * system.dt
+        self.p = p + v * system.dt + 0.5 * R @ (system.a - b_a) * system.dt ** 2 + 0.5 * system.g * system.dt ** 2
 
-        A = self.compute_A(system)
-        print("A: ", A)
-        self.error_estimated = self.error_estimated @ expm(A * system.dt)
+        # A = self.compute_A(system)
+        # self.error_estimated = self.error_estimated @ expm(A * system.dt)
 
     def propagation_covariance(self, system):
         PHI_k = expm(self.compute_A(system) * system.dt) # 21x21
@@ -99,17 +101,30 @@ class Right_IEKF:
         PI = self.compute_PI()
         b = np.array([0, 0, 0, -1, 0])
 
-        print("K.shape: ", np.shape(K))
-        print("PI.shape: ", np.shape(PI))
-        print("self.compute_X().shape: ", np.shape(self.compute_X()))
-        print("sys.y_encoder.shape: ", np.shape(sys.y_encoder))
-        print("b.shape: ", np.shape(b))
-        delta = K @ PI @ (self.compute_X() @ sys.y_encoder - b)
-        delta_IMU = delta[0:9]
-        delta_bw = delta[9:12]
-        delta_ba = delta[12:15]
-        delta_Rc = delta[15:18]
-        delta_pc = delta[18:21]
+        delta_1 = K @ PI @ (self.compute_X() @ sys.y_encoder_1 - b)
+
+        delta_IMU = delta_1[0:9]
+        delta_bw = delta_1[9:12]
+        delta_ba = delta_1[12:15]
+        delta_Rc = delta_1[15:18]
+        delta_pc = delta_1[18:21]
+        X = expm(wedge(delta_IMU)) @ self.compute_X()
+        self.b_w = self.b_w + delta_bw
+        self.b_a = self.b_a + delta_ba
+        self.R_c = expm(skew(delta_Rc)) @ self.R_c
+        self.p_c = self.p_c + delta_pc
+        self.P = (np.eye(21) - K @ H) @ self.P @ (np.eye(21) - K @ H).T + K @ N @ K.T
+        self.R = X[0:3, 0:3]
+        self.v = X[0:3, 3]
+        self.p = X[0:3, 4]
+
+        delta_2 = K @ PI @ (self.compute_X() @ sys.y_encoder_2 - b)
+
+        delta_IMU = delta_2[0:9]
+        delta_bw = delta_2[9:12]
+        delta_ba = delta_2[12:15]
+        delta_Rc = delta_2[15:18]
+        delta_pc = delta_2[18:21]
         X = expm(wedge(delta_IMU)) @ self.compute_X()
         self.b_w = self.b_w + delta_bw
         self.b_a = self.b_a + delta_ba
@@ -123,36 +138,83 @@ class Right_IEKF:
     def measurement_model_camera(self, sys):
         v_c_estimated = self.R_c.T @ self.R.T @ self.v + skew(sys.w_c_observation) @ self.R_c.T @ self.p_c
         inno_error = v_c_estimated - sys.v_c_observation
+        print("v_c_observation: ", sys.v_c_observation)
+        print("inno_error: ", inno_error)
+
         ## Jacobina matrix H and G
         H = np.zeros((3, 21))
         H[:, 3:6] = self.R_c.T @ self.R.T
-        H[:, 15:18] = -skew(self.R_c @ self.R.T @ self.v)-skew(sys.w_c_observation) @ skew(self.R_c.T @ self.p_c)
-        H[:, 18:21] = skew(sys.w_c_observation) @ self.R_c.T
+        H[:, 15:18] = self.R_c.T @ skew(self.R.T @ self.v)+skew(sys.w_c_observation) @ self.R_c.T @ skew(self.p_c)
+        H[:, 18:21] = -skew(sys.w_c_observation) @ self.R_c.T
         G = np.zeros((3, 6))
         G[:, 0:3] = -skew(self.R_c.T @ self.p_c)
         G[:, 3:6] = -np.eye(3)
         ##
+
+        N = sys.N_camera[3:6, 3:6] + skew(self.R_c.T @ self.p_c) * sys.N_camera[0:3, 0:3] @ skew(self.R_c.T @ self.p_c).T
+        N = self.R @ self.R_c @ N @ (self.R @ self.R_c).T
+
+        # S = H @ self.P @ H.T + N  ## S is different
         S = H @ self.P @ H.T + G @ sys.N_camera @ G.T
         K = self.P @ H.T @ np.linalg.inv(S)
-        error = self.error_estimated + K @ inno_error
+        error = K @ inno_error
+
         self.P = (np.eye(21) - K @ H) @ self.P
+        # self.P = (np.eye(21) - K @ H) @ self.P @ (np.eye(21) - K @ H).T + K @ G @ sys.N_camera @ G.T @ K.T
 
-        error_R = error[0:3]
-        error_v = error[3:6]
-        error_p = error[6:9]
-        error_b_w = error[9:12]
-        error_b_a = error[12:15]
-        error_R_c = error[15:18]
-        error_p_c = error[18:21]
+        delta_IMU = error[0:9]
+        delta_bw = error[9:12]
+        delta_ba = error[12:15]
+        delta_Rc = error[15:18]
+        delta_pc = error[18:21]
+        X = expm(wedge(delta_IMU)) @ self.compute_X()
+        self.b_w = self.b_w + delta_bw
+        self.b_a = self.b_a + delta_ba
+        self.R_c = expm(skew(delta_Rc)) @ self.R_c
+        self.p_c = self.p_c + delta_pc
+        self.R = X[0:3, 0:3]
+        self.v = X[0:3, 3]
+        self.p = X[0:3, 4]
 
-        R = self.R
-        self.R = self.R @ expm(skew(error_R))
-        self.v = self.R @ R.T @ (self.v - error_v)
-        self.p = self.R @ R.T @ (self.p - error_p)
-        self.b_w = self.b_w - error_b_w
-        self.b_a = self.b_a - error_b_a
-        self.R_c = self.R_c @ expm(skew(error_R_c))
-        self.p_c = self.p_c - error_p_c
+
+        # error_wedge = wedge(error[0:9])
+        # X = np.zeros((5, 5))
+        # X[0:3, 0:3] = self.R
+        # X[0:3, 3] = self.v
+        # X[0:3, 4] = self.p
+        # X[3, 3] = 1
+        # X[4, 4] = 1
+        # X = expm(error_wedge) @ X
+        # self.R = X[0:3, 0:3]
+        # self.v = X[0:3, 3]
+        # self.p = X[0:3, 4]
+        # self.b_w = self.b_w + error_b_w
+        # self.b_a = self.b_a + error_b_a
+        # self.R_c = expm(skew(error_R_c)) @ self.R_c
+        # self.p_c = self.p_c + error_p_c
+
+
+        # error_R = error[0:3]
+        # error_v = error[3:6]
+        # error_p = error[6:9]
+        # error_b_w = error[9:12]
+        # error_b_a = error[12:15]
+        # error_R_c = error[15:18]
+        # error_p_c = error[18:21]
+        #
+        # R = self.R  ## R is estimated value
+        # self.R = expm(skew(error_R)) @ self.R ## self.R is true value right now
+        # self.v = self.R @ R.T @ (self.v - error_v)
+        # self.p = self.R @ R.T @ (self.p - error_p)
+        # # self.v = self.v + error_v
+        # # self.p = self.p + error_p
+        # self.b_w = self.b_w + error_b_w
+        # self.b_a = self.b_a + error_b_a
+        # self.R_c = expm(skew(error_R_c)) @ self.R_c
+        # self.p_c = self.p_c + error_p_c
+
+def measurement_model_position(self, sys):
+
 
 
 
